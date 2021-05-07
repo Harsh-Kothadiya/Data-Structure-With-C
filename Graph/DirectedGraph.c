b8c27dcbebff02a8e81b105abfbd6f5bee8910a4
KOTHADIYA <54473484+KOTHADIYA@users.noreply.github.com>
2021-05-07 12:56:37 +0530

    Add files via upload

---
+++ Graph/DirectedGraph.c
@@ -0,0 +1,77 @@
+//DIRECTED GRAPH WITH ADJACENCY MATRIX
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+struct Graph
+{
+    int V;
+    int E;
+    int **Adj;
+};
+void createGraph(int,int);
+void main()
+{
+    int v,e;
+    printf("\n\n\t ENTER TOTAL NUMBER OF VERTEX(NODE) : ");
+    scanf("%d",&v);
+    printf("\n\n\t ENTER TOTAL NUMBER OF EDGES : ");
+    scanf("%d",&e);
+
+    createGraph(v,e);
+    printf("\n\n");
+    getch();
+}
+void createGraph(int v,int e)
+{
+    struct Graph *G;
+    G=(struct Graph*)malloc(sizeof(struct Graph));
+    G->V=v;
+    G->E=e;
+    G->Adj=(int**)malloc(sizeof(int*)*v);
+    int i,j,k;
+    for(i=0;i<v;i++)
+        G->Adj[i]=(int*)malloc(sizeof(int*)*v);
+
+    //now all value is zero in matrix
+    for(i=0;i<v;i++)
+    {
+        for(j=0;j<v;j++)
+        {
+            G->Adj[i][j]=0;
+        }
+    }
+    printf("\n\n----------INITIAL DIRECTED GRAPH----------------------\n\n");
+    for(i=0;i<v;i++)
+        printf("\tv%d",i+1);
+    printf("\n------------------------------------------\n");
+    for(i=0;i<v;i++)
+    {
+        printf("v%d|",i+1);
+        for(j=0;j<v;j++)
+        {
+            printf("\t%d",G->Adj[i][j]);
+        }
+        printf("\n");
+    }
+    for(k=0;k<e;k++)
+    {
+        printf("\n\n\t ENTER VERTEX NUMBER CONNECTED BY THIS EDGES : ");
+        scanf("%d%d",&i,&j);
+        G->Adj[i-1][j-1]=1;
+    }
+    printf("\n\n----------AFTER DATA ENTER DIRECTED GRAPH----------------------\n\n");
+    for(i=0;i<v;i++)
+        printf("\tv%d",i+1);
+    printf("\n-------------------------------------------\n");
+    for(i=0;i<v;i++)
+    {
+        printf("v%d|",i+1);
+        for(j=0;j<v;j++)
+        {
+            printf("\t%d",G->Adj[i][j]);
+        }
+        printf("\n");
+    }
+}
+
